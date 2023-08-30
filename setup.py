1fd716831655ee9bcfd19c352c5c68456e85f22a
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2023-08-30 20:47:04 +0530

    Fix Workflow and prepare for v0.0.1 release

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
+import os
 
 
 def read_requirements(file_name):
+    file_name = os.path.abspath(file_name)  # Convert to absolute path
+    requirements = []
     with open(file_name, 'r') as file:
-        return [line.strip() for line in file if line.strip()]
-
+        for line in file:
+            line = line.strip()
+            if line.startswith('-r') or line.startswith('--requirement'):
+                # Recursively read referenced requirements file
+                referenced_file = line.split(maxsplit=1)[1]
+                requirements.extend(read_requirements(os.path.join(os.path.dirname(file_name), referenced_file)))
+            elif line and not line.startswith('#'):  # Ignore comment lines
+                requirements.append(line)
+    return requirements
 
 setup(
     name='xpuls-mlmonitor',
@@ -17,7 +27,7 @@ setup(
     packages=find_packages(),
     install_requires=read_requirements('requirements.txt'),
     extras_require={
-        'langchain': read_requirements('requirements/langchain_requirements.txt'),
+        'langchain': read_requirements('requirements/requirements_langchain.txt'),
         'all': read_requirements('requirements.txt')
     }
 )
