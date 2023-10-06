07875c9fca90b152f71a791c8437f23c26c79d14
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2023-10-06 20:51:07 +0530

    fix: map to default project when not mapped

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='xpuls-mlmonitor',
-    version='0.1.0',
+    version='0.1.1',
     author='Sai Sharan Tangeda',
     author_email='saisarantangeda@gmail.com',
     description='Automated telemetry and monitoring for ML & LLM Frameworks',
